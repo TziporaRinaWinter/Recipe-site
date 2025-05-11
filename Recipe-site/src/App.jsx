ca9725b3316b6f6c4335e84485e847f69de45eea
TziporaRinaWinter <tzr65055@gmail.com>
2025-05-11 20:04:23 +0300

    Header to the recipeList

--- Recipe-site/src/App.jsx
+++ Recipe-site/src/App.jsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import RecipeCardList from "./components/RecipeListDisplay/RecipeCardList.jsx";
+import CategoryRecipesPage from "./components/RecipeListDisplay/CategoryRecipesPage.jsx";
 
 function App() {
-  const recipe = {
-    id: "1",
-    image: "./src/assets/לחם הבית.jpg",
-    title: "שם המתכון",
-    preparationTime: "30 דקות",
-    category: "מאפה",
-    rank: "קל",
-    remark:
-      "ננחגמלגנכ כה כג המנהכחלגנ  ינדנ הגלנכמד  וידחכנג  נכגנכ גנלד ה דכלה חדעדנהנחנהחלגד החנלדכנדהד",
-    instructions: "הוראות הכנה...",
-  };
-
   return (
     <>
-      <RecipeCardList recipes={[recipe, recipe, recipe, recipe]} />
+      <CategoryRecipesPage></CategoryRecipesPage>
     </>
   );
 }
---
+++ Recipe-site/src/components/RecipeListDisplay/CategoryRecipesPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import RecipeCardList from "./RecipeCardList";
+// import allRecipes from "../data/recipes"; // כל המתכונים (לדוגמה)
+// import { useParams } from "react-router-dom"; // אם יש קטגוריות ב-URL
+
+function CategoryRecipesPage() {
+  //   const { categoryName } = useParams(); //  /recipes/:categoryName
+  const categoryName = "מאפים";
+
+  //   const recipesInCategory = allRecipes.filter(
+  //     (r) => r.category === categoryName
+  //   );
+
+  //   const filteredRecipes = recipesInCategory.filter((recipe) =>
+  //     recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //   );
+  const recipe = {
+    id: "1",
+    image: "./src/assets/לחם הבית.jpg",
+    title: "שם המתכון",
+    preparationTime: "30 דקות",
+    category: "מאפה",
+    rank: "קל",
+    remark:
+      "ננחגמלגנכ כה כג המנהכחלגנ  ינדנ הגלנכמד  וידחכנג  נכגנכ גנלד ה דכלה חדעדנהנחנהחלגד החנלדכנדהד",
+    instructions: "הוראות הכנה...",
+  };
+
+  const categoryImages = {
+    פסטות: "/images/pasta.jpg",
+    סלטים: "/images/salad.jpg",
+    מרקים: "/images/soups.jpg",
+    מאפים: "./src/assets/לחם הבית.jpg",
+  };
+
+  return (
+    <div>
+      <RecipeCardList
+        recipes={[recipe, recipe, recipe, recipe, recipe]}
+        category={categoryName}
+        imageSrc={categoryImages[categoryName]}
+      />
+    </div>
+  );
+}
+
+export default CategoryRecipesPage;
