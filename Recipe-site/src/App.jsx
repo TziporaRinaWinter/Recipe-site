11505d04cdc07694134b050e18b23ac20afea40b
TziporaRinaWinter <tzr65055@gmail.com>
2025-05-11 20:29:35 +0300

    import---

--- Recipe-site/src/App.jsx
+++ Recipe-site/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import CategoryRecipesPage from "./components/RecipeListDisplay/CategoryRecipesPage.jsx";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
